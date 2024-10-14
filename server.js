f5cf76ce08de109171e764fd8229940eb40dbcad
Guillaume Sere <guillaumesere@hotmail.fr>
2024-10-14 14:55:42 +0200

    deployment

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const corsOptions = {
 
     if (!origin) return callback(null, true);
 
-    const allowedOrigins = ['http://localhost:3000', 'https://localhost:8000'];
+    const allowedOrigins = process.env.FRONTEND_URL || 'http://localhost:3000';
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
